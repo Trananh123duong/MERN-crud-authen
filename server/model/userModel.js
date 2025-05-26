8da1c15fa7768acfc833140aaeaf81a134b5be48
dev05 <dev05@dev.co-mit.com>
2025-05-26 16:23:57 +0700

    Set user model

---
+++ server/model/userModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    adress: {
+        type: String,
+        require: true
+    }
+})
+
+export default mongoose.model("Users", userSchema)
