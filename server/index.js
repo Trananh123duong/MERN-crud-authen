c14dd30edc73c6763d0f063d9555dbc271c18ab9
dev05 <dev05@dev.co-mit.com>
2025-05-27 11:44:41 +0700

    create user

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRoute from "./routes/userRoute.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,3 +20,5 @@ mongoose
     });
   })
   .catch((error) => console.log(error));
+
+app.use("/api/user", userRoute);
