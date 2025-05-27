7f1bfbbe799e833f12051cdfec69ef2bcf658f94
dev05 <dev05@dev.co-mit.com>
2025-05-27 14:41:52 +0700

    Detail user

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -28,3 +28,16 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({errorMessage:error.message})
   }
 }
+
+export const getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userExist = await User.findById(id);
+    if (!userExist) {
+      return res.status(400).json({message: "User not found."});
+    }
+    res.status(200).json(userExist);
+  } catch (error) {
+    res.status(500).json({errorMessage:error.message})
+  }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { create, getAllUsers } from "../controller/userController.js";
+import { create, getAllUsers, getUserById } from "../controller/userController.js";
 
 const route = express.Router();
 
 route.post("/", create)
 route.get("/", getAllUsers)
+route.get("/:id", getUserById)
 
 export default route;
